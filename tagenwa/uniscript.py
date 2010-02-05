bee9b5e54466c0f0bc64ade493adf2c173966e28
Eric Dessalle <eric.dessalle@gmail.com>
2010-02-05 11:17:31 +0900

    Determine unicode blocks and scripts from UCD 5.1.0

--- tagenwa/uniscript.py
+++ tagenwa/uniscript.py
@@ -6,54 +6,86 @@ Unicode script helper functions
 __version__ = "0.1"
 __license__ = "MIT"
 
+from unicodedata import name
+from codecs import open
+from os.path import abspath, dirname, join as joinpath
+import re
 
 def islatin(text):
 	"""Return True if all characters are latin and there is at least one character, False otherwise."""
-	return len(text) and all(script(c) == u'latin' for c in text)
+	return len(text) and all(script(c) == 'LATIN' for c in text)
+
+
+def block(c):
+	"""Return the Unicode block name of the character.
+	
+	The data is based on the Unicode 5.1.0 database:
+	http://www.unicode.org/Public/5.1.0/ucd/Blocks.txt
+	
+	:param c: single character
+	:type c: unicode
+	:return: block name
+	:rtype: str
+	:raise TypeError: if the argument is not a single unicode character.
+	:raise KeyError: if the character block is not found.
+	"""
+	# assert argument is a single unicode character
+	if not isinstance(c, unicode):
+		raise TypeError('block() argument must be unicode, not '+repr(type(c)))
+	if len(c) != 1:
+		raise TypeError('block() argument must be a single unicode character')
+	
+	o = ord(c)
+	for a, b, blockname in _UCD_BLOCKS:
+		if a <= o <= b:
+			return blockname
+	raise KeyError('Unicode block not found.')
 
 
 def script(c):
-	"""Return the script type of the character if known, None otherwise.
+	"""Return the script of the character if known, None otherwise.
+	
+	The data is based on the Unicode 5.1.0 database:
+	http://www.unicode.org/Public/5.1.0/ucd/Scripts.txt
 	
 	:param c: character
-	:type c: unicode character
-	:return: a unicode string representing the script
-	:rtype: unicode or None
+	:type c: unicode
+	:return: script name
+	:rtype: str
+	:raise TypeError: if the argument is not a single unicode character
+	:raise KeyError: if the character script is not found.
 	"""
+	# assert argument is a single unicode character
+	if not isinstance(c, unicode):
+		raise TypeError('block() argument must be unicode, not '+repr(type(c)))
+	if len(c) != 1:
+		raise TypeError('block() argument must be a single unicode character')
+	
 	o = ord(c)
-	if o <= 0x02AF \
-		or 0x1D00 <= o <= 0x1DBF \
-		or 0x1E00 <= o <= 0x1EFF \
-		or 0x2C60 <= o <= 0x2C7F \
-		or 0xA720 <= o <= 0xA7FF \
-		or 0xFB00 <= o <= 0xFB4F \
-		or 0xFF01 <= o <= 0xFF5E:
-		# see http://en.wikipedia.org/wiki/Latin_characters_in_Unicode
-		# basic latin, latin supplement, latin extended A&B, IPA
-		# phonetic extension
-		# latin extended additional
-		# latin extended C
-		# latin extended D
-		# fullwidth latin
-		return u'latin'
-	elif 0x0374 <= o <= 0x03FF:
-		return u'greek'
-	elif 0x0400 <= o <= 0x0523:
-		return u'cyrillic'
-	elif 0x0E00 <= o <= 0x0E7F:
-		return u'thai'
-	elif 0x4E00 <= o <= 0x9FFF  \
-		or 0x3400 <= o <= 0x4DBF \
-		or 0x20000 <= o <= 0x2A6DF \
-		or 0x2A700 <= o <= 0x2B73F \
-		or 0xF900 <= o <= 0xFAFF:
-		#CJK, CJK A, CJK B, CJK C, CJK compatibility
-		return u'cjk'
-	elif 0x3040 <= o <= 0x309F:
-		return u'hiragana'
-	elif 0x30A0 <= o <= 0x30FF:
-		return u'katakana'
-	elif 0xAC00 <= o <= 0xD7AF:
-		return u'hangul'
-	else:
-		return None
+	for a, b, scriptname in _UCD_SCRIPTS:
+		if a <= o <= b:
+			return scriptname
+	raise KeyError('Unicode script not found.')
+
+
+_ucd_pattern = re.compile(r'(?P<start>[0-9A-F]+)(?:\.\.(?P<end>[0-9A-F]+))? *;(?P<value>[^#]*)(?:#|\n)')
+def _read_ucd_datafile(filename, folder='ucd510'):
+	"""Read UCD data file."""
+	filepath = joinpath(abspath(dirname(__file__)),folder,filename)
+	data = []
+	with open(filepath, 'rU', encoding='latin1') as f:
+		for line in f:
+			if line and not line.startswith('#'):
+				match = _ucd_pattern.match(line)
+				if match:
+					start = int(match.group('start'),16)
+					end = int(match.group('end'),16) if match.group('end') else start
+					value = match.group('value').strip()
+					data.append((start,end,value))
+	return data
+
+_UCD_BLOCKS = _read_ucd_datafile('Blocks.txt')
+_UCD_SCRIPTS = _read_ucd_datafile('Scripts.txt')
+
+from pprint import pprint
+pprint(_UCD_BLOCKS)
