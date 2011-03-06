8f3ba7809c743978e04876fc4a03ee84a28daa19
Eric Dessalle <eric.dessalle@gmail.com>
2011-03-07 03:05:47 +0900

    Add a NgramLanguageClassifier

--- tagenwa/langid/ngram.py
+++ tagenwa/langid/ngram.py
@@ -1,34 +1,47 @@
 # -*- coding: UTF-8 -*-
 """
-Single word n-gram language identifier
+Character n-gram language classifier
 
 """
-__version__ = "0.1"
 __license__ = "MIT"
 
 from collections import defaultdict
-from math import log, log1p
+from math import log, exp
+import unicodedata
+import itertools
+import re
 
-from tagenwa.util.tools import sliding_tuples
+from nltk.classify.api import ClassifierI
+from nltk.probability import FreqDist, ELEProbDist, DictionaryProbDist
+
+from tagenwa.utils import sliding_tuples
+from tagenwa.tag.hmm import ClassifierBasedHMMTagger
 
 
-def ngram_generator_factory(n):
-	"""Return an function returning a list of n-grams of a token.
+
+###############################################################################
+# N-gram
+###############################################################################
+
+def build_ngram_function(n):
+	"""Return an function returning a list of character n-grams from a string
+	for use in language identification.
 	
-	The returned function accepts a Token as parameter and returns a list of n-grams as Unicode strings.
+	The returned function accepts a Unicode string as parameter
+	and returns a list of n-grams as Unicode strings.
 	N-grams where the middle character(s) are not in the token are filtered out
-	to avoid over-representing characters in the beginning and the end 	of the word.
-	When the n-gram is not completely contained in the token, None is used as a place-holder character.
+	to avoid over-representing characters in the beginning and the end of the word.
+	When the n-gram is not completely contained in the token, a space is used
+	as a place-holder character.
 	
-	>>> from tagenwa.token import Token
-	>>> f = ngram_generator_factory(3)
-	>>> f(Token(u'abc'))
-	[(None, u'a', u'b'), (u'a', u'b', u'c'), (u'b', u'c', None)]
+	>>> get_trigrams = build_ngram_function(3)
+	>>> get_trigrams(u'Abc')
+	[u' ab', u'abc', u'bc ']
 	
 	:param n: size of the n-grams
 	:type n: int
 	:return: function returning a list of n-grams
-	:rtype: function(Token)
+	:rtype: function(unicode)
 	"""
 	# Assert that n is a strictly positive integer
 	assert(isinstance(n, int) and n > 0)
@@ -39,177 +52,176 @@ def ngram_generator_factory(n):
 	else:
 		check_not_none = [(n - 1) / 2] if n % 2 else [n / 2 -1, n / 2]
 	
-	def ngram_generator(token):
-		return [tu for tu in sliding_tuples(token.text, n) if all(tu[i] is not None for i in check_not_none)]
-	return ngram_generator
+	def get_ngrams(token):
+		if _SPACEPUNCT.match(token) is not None:
+			# If space or punctuation, don't return any n-grams
+			return []
+		return [u''.join(tu) for tu in sliding_tuples(token.lower(), n, fill_value=u' ') if all(tu[i] != u' ' for i in check_not_none)]
+	return get_ngrams
 
 
 
-class NgramLanguageIdentifier(object):
-	"""Single word n-gram language identifier
+###############################################################################
+# Feature set
+###############################################################################
+
+_SPACEPUNCT = re.compile(ur'(\s+|\W)', re.U)
+
+def tokenize(text, normalization_form='NFC'):
+	"""Tokenize the text on spaces and punctuations"""
+	text = unicodedata.normalize(normalization_form, text)
+	return [t for t in _SPACEPUNCT.split(text) if t]
+
+
+
+###############################################################################
+# N-gram language classifier
+###############################################################################
+
+class NgramLanguageTraining(object):
+	
+	def __init__(self, n=None, ngram_function=None, tokenize_function=None):
+		"""Create a new training instance for a n-gram language classifier."""
+		self._freqdists = {}
+		
+		# Define the n-gram function
+		if ngram_function is not None:
+			self._ngrams = ngram_function
+		elif n is not None:
+			self._ngrams = build_ngram_function(n)
+		else:
+			raise ValueError('Either the argument n or the argument ngram_function must be specified.')
+		
+		# Define the tokenize function
+		self._tokenize = tokenize_function if tokenize_function is not None else tokenize
+	
+	
+	def add(self, text, language):
+		"""Add the given text to the training data"""
+		# Create the frequency distribution if it doesn't exist yet
+		if language not in self._freqdists:
+			self._freqdists[language] = FreqDist()
+		freqdist = self._freqdists[language]
+		
+		# Update the frequency distribution
+		tokens = self._tokenize(text)
+		for token in tokens:
+			freqdist.update(self._ngrams(token))
+	
+	
+	def add_corpus(self, corpus, language):
+		"""Add the given corpus to the training data"""
+		for fileid in corpus.fileids():
+			self.add(corpus.raw(fileid), language)
+	
+	
+	def get_freqdists(self):
+		"""Return the frequency distributions learned from the training data"""
+		return self._freqdists
+	
+	
+	def get_probdists(self):
+		"""Return the probability distributions learned from the training data"""
+		return dict((lang, ELEProbDist(self._freqdists[lang])) for lang in self._freqdists)
+	
+
+
+class NgramLanguageClassifier(ClassifierI):
+	"""A n-gram language classifier.
 	
 	"""
 	
-	def __init__(self, n, ngram_generator=None, prior=0.0, smoothing_coefficient=0.0):
-		"""Create a single word n-gram language identifier.
-		
-		If `ngram_generator` is None, the n-gram generator function is created automatically
-		using the `ngram_generator_factory` function with the parameter `n`.
-		
-		The parameters `prior` and `smoothing_coefficient` define the parameters of the
-		`beta-distributed <http://en.wikipedia.org/wiki/Beta_distribution>`_
-		`conjugate prior <http://en.wikipedia.org/wiki/Conjugate_prior>`_
-		used in the estimation of the language probabilities.
-		They are linked to the parameters alpha and beta of the prior's beta distribution
-		as follows:
-		
-		* alpha = `prior` * `smoothing_coefficient`
-		* beta = (1 - `prior`) * `smoothing_coefficient`
-		
-		with 0 <= `prior` <= 1 and 0 <= `smoothing_coefficient`.
-		The mean of this beta distribution is equal to `prior`.
-		The `smoothing_coefficient` corresponds to the number of observations
-		that would be needed to get a posterior beta distribution
-		with the same parameters alpha and beta
-		starting from a uniform distributed prior.
-		
-		:param n: the size of the n-grams
-		:type n: int
-		:param ngram_generator: a function that accepts a Token and return a list of n-grams (default: None)
-		:type ngram_generator: function(Token)
-		:param prior: the a priori probability
-		:type prior: float
-		:param smoothing_coefficient: the number of observations of the a priori probability
-		:type smoothing_coefficient: float
+	def __init__(self, training, cutoff=None):
 		"""
-		self.n = n
-		self.frequencies = {}
-		self.frequency_totals = {}
-		self.log_frequencies = {}
-		self.ngram_generator = ngram_generator if ngram_generator else ngram_generator_factory(n)
-		self.prior = prior
-		self.smoothing_coefficient = smoothing_coefficient
-	
-	def get_known_languages(self):
-		"""Return the languages on which the identifier has been trained.	
-		
-		:rtype: list
+		:param training: the training data for the language classification.
+		:type training: NgramLanguageTraining
 		"""
-		return self.frequencies.keys()
-	
-	def ngrams(self, token):
-		"""Return the n-grams of the token.
-		
-		:param token: the token for which the n-grams are generated
-		:type token: tagenwa.token.Token
-		:return: a list of n-grams as Unicode strings
-		:rtype: list
-		"""
-		return self.ngram_generator(token)
-	
-	def train(self, lang, tokens, process_training=True):
-		"""Train the language identifier on the tokens.
-		
-		This method calls `train_frequency` behind the scene.
-		
-		:param lang: the language
-		:param tokens: iterable of tokens to train on
-		:param process_training: specifies whether to build the cache at the end of the training
-		:type process_training: bool
-		:return: None
-		"""
-		# Generate the ngram frequency
-		frequency = defaultdict(int)
-		for t in tokens:
-			for ngram in self.ngram_generator(t.text):
-				frequency[ngram] += 1
-		# Train
-		return self.train_frequency(lang, frequency, process_training)
+		self._training = training
+		self._probdists = training.get_probdists()
+		self._languages = self._probdists.keys() + [u'und']
+		self._cutoff = cutoff
 	
 	
-	def train_frequency(self, lang, frequency, process_training=True):
-		"""Train the language identifier on the n-gram frequency.
-		
-		:param lang: the language
-		:param frequency: dictionary of n-gram frequencies to train on
-		:type frequency: dict
-		:param process_training: specifies whether to build the cache at the end of the training
-		:type process_training: bool
-		:return: None
-		"""
-		# Invalidate the cache
-		self.log_frequencies = {}
-		# Add language if not existing
-		if lang not in self.frequencies:
-			self.frequencies[lang] = {}
-			self.frequency_totals[lang] = 0
-		# Update the ngrams frequencies
-		frequencies_lang = self.frequencies[lang]
-		for ngram in frequency:
-			frequencies_lang[ngram] = frequencies_lang.get(ngram, 0) + frequency[ngram]
-		# Update the total frequency
-		self.frequency_totals[lang] += sum(frequency.itervalues())
-		# Update the cache
-		if process_training:
-			self.process_training()
+	def labels(self):
+		"""Return the list of category labels used by this classifier."""
+		return self._languages
 	
 	
-	def estimate(self, token):
-		"""Estimate the log probability of each known language for the token.
+	def score_classify(self, featureset):
+		"""Return a dict of scores (unnormalized probability estimates) for each language for the given featureset."""
 		
-		This method calls `estimate_ngrams` with the results of the `ngrams` method as parameter.
+		if u'ngrams' not in featureset:
+			raise ValueError('The feature set does not contain a feature named "ngrams".')
 		
-		:param token: the token to estimate
-		:type token: tagenwa.token.Token
-		:return: dictionary of the log probability for each language
-		:rtype: dict
-		"""
-		ngrams = self.ngram_generator(token)
-		return self.estimate(ngrams)
+		# Calculate the score of each language as the unnormalized join probability of each n-gram
+		scores = {}
+		for lang in self._probdists:
+			probdist = self._probdists[lang]
+			scores[lang] = exp(sum(probdist.logprob(ngram) for ngram in featureset[u'ngrams']))
+		# Add a score for the "undetermined language"
+		if self._cutoff:
+			scores[u'und'] = pow(self._cutoff, len(featureset[u'ngrams']))
+		else:
+			scores[u'und'] = min(scores.values())
+		return scores
 	
 	
-	def estimate_ngrams(self, ngrams):
-		"""Estimate the log probability of each known language for the list of n-grams.
-		
-		:param ngrams: the n-grams to estimate
-		:type ngrams: list
-		:return: dictionary of log probability for each language
-		:rtype: dict
-		"""
-		if not ngrams:
-			# All languages are equally probable
-			return dict((lang, 0.0) for lang in self.frequencies)
-		
-		assert self.log_frequencies, 'Please first initialize the cache by running the build_cache() method'
-		
-		# Precalculate prior
-		alpha = self.smoothing_coefficient * self.prior
-		beta = self.smoothing_coefficient * (1.0 - self.prior)
-		log1p_alpha = log1p(alpha)
-		length = len(ngrams)
-		
-		estimates = {}
-		for lang in self.frequencies:
-			log_frequencies_lang_get = self.log_frequencies[lang].get
-			estimates[lang] = (
-				sum(log_frequencies_lang_get(ngram, log1p_alpha) for ngram in ngrams)
-				- log(beta + self.frequency_totals[lang]) * length
-			)
-		return estimates
+	def prob_classify(self, featureset):
+		"""Return a probability distribution over labels for the given featureset."""
+		scores = self.score_classify(featureset)
+		return DictionaryProbDist(scores, normalize=True)
 	
+	
+	def classify(self, featureset):
+		"""Return the most appropriate label for the given featureset."""
+		return self.prob_classify(featureset).max()
+	
+	
+	def get_token_featureset(self, token):
+		"""Return a featureset corresponding to the given token"""
+		return {
+			u'text': token,
+			u'ngrams': self._training._ngrams(token),
+		}
+	
+	
+	def prob_classify_text(self, text):
+		"""Return a probability distribution over labels for the given text."""
+		tokens = self._training._tokenize(text)
+		ngrams = itertools.chain.from_iterable(
+			self._training._ngrams(token) for token in tokens
+		)
+		featureset = {
+			u'text': text,
+			u'ngrams': list(ngrams),
+		}
+		return self.prob_classify(featureset)
+	
+	
+	def classify_text(self, text):
+		"""Return the most appropriate label for the given text."""
+		return self.prob_classify_text(featureset).max()
 
-	def process_training(self):
-		"""Process the training.
-		
-		A log frequency cache is needed when running the estimate() methods and
-		needs to be built after the model has been trained.
-		
-		:return: None
-		"""
-		alpha = self.smoothing_coefficient * self.prior
-		self.log_frequencies = {}
-		for lang in self.frequencies:
-			frequencies_lang = self.frequencies[lang]
-			self.log_frequencies[lang] = dict(
-				(ngram, log1p(alpha + frequencies_lang[ngram])) for ngram in frequencies_lang
-			)
+
+
+###############################################################################
+# N-gram language classifier
+###############################################################################
+
+class NgramHMMLanguageTagger(ClassifierBasedHMMTagger):
+	
+	def __init__(self, classifer, loginit=None, logtrans=None, *args, **kwargs):
+		self._classifer = classifier
+		self.states = self._classifer.labels()
+		if loginit is not None:
+			self.loginit = loginit	
+		if logtrans is not None:
+			self.logtrans = logtrans
+	
+	def logtrans(self, featureset1, featureset2):
+		if not featureset2[u'ngrams']:
+			return dict(((s1, s2), 0.0 if s1==s2 else -15.0) for s1 in self.states for s2 in self.states)
+		elif not featureset1[u'ngrams']:
+			return dict(((s1, s2), 0.0 if s1==s2 else -20.0) for s1 in self.states for s2 in self.states)
+		else:
+			return dict(((s1, s2), 0.0 if s1==s2 else -10.0) for s1 in self.states for s2 in self.states)
