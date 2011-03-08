1e6eb6f75c333623b2bad6a66f3873e2d6fb0b60
Eric Dessalle <eric.dessalle@gmail.com>
2011-03-08 22:54:26 +0900

    Rename the "uniscript" module to "unicodescript"

--- test/test_all.py
+++ test/test_all.py
@@ -1,8 +1,8 @@
 # -*- coding: UTF-8 -*-
 import unittest
 
-import test_tools
-import test_uniscript
+import test_utils
+import test_unicodescript
 import test_token
 import test_pre
 import test_ngram
@@ -11,8 +11,8 @@ import test_retokenizer_dictionary
 import test_retokenizer_number
 
 all_tests = unittest.TestSuite([
-	test_tools.suite(),
-	test_uniscript.suite(),
+	test_utils.suite(),
+	test_unicodescript.suite(),
 	test_token.suite(),
 	test_pre.suite(),
 	test_ngram.suite(),
--- test/test_unicodescript.py
+++ test/test_unicodescript.py
@@ -1,20 +1,20 @@
 # -*- coding: UTF-8 -*-
 import unittest, doctest
 
-from tagenwa.uniscript import script, block
+from tagenwa.unicodescript import script, block
 
 
 class TestScript(unittest.TestCase):
 	
-	def test_uniscript_doctest(self):
+	def test_unicodescript_doctest(self):
 		"""Test uniscript doctests"""
-		import tagenwa.uniscript
-		failure_count, test_count = doctest.testmod(tagenwa.uniscript)
-		self.assertEqual(failure_count, 0, 'Testing doctest from tagenwa.uniscript: %i failed out of %i' % (failure_count, test_count))
+		import tagenwa.unicodescript
+		failure_count, test_count = doctest.testmod(tagenwa.unicodescript)
+		self.assertEqual(failure_count, 0, 'Testing doctest from tagenwa.unicodescript: %i failed out of %i' % (failure_count, test_count))
 	
 	
 	def test_block(self):
-		"""Test uniscript.block()"""
+		"""Test unicodescript.block()"""
 		testcases = [
 			(u'a','Basic Latin'),
 			(u'A','Basic Latin'),
@@ -36,40 +36,40 @@ class TestScript(unittest.TestCase):
 			self.assertEqual(block(i),e)
 	
 	def test_block_error(self):
-		"""Test uniscript.block() with invalid argument"""
+		"""Test unicodescript.block() with invalid argument"""
 		self.assertRaises(TypeError, block, (5,))
 		self.assertRaises(TypeError, block, ('a',))
 		self.assertRaises(TypeError, block, (u'abc',))
 	
 	def test_script_error(self):
-		"""Test uniscript.script() with invalid argument"""
+		"""Test unicodescript.script() with invalid argument"""
 		self.assertRaises(TypeError, script, (5,))
 		self.assertRaises(TypeError, script, ('a',))
 		self.assertRaises(TypeError, script, (u'abc',))
 	
 	def test_latin_letters(self):
-		"""Test uniscript.script() with Latin"""
+		"""Test unicodescript.script() with Latin"""
 		testcases = u'aAåÅăĂąĄẬậɑⱭɐⱯæÆéÉèÈȄȅḿḾøØœŒÿŸ'
 		for c in testcases:
 			self.assertEqual(script(c, avoid_common=True), 'Latin')
 			self.assertEqual(script(c, avoid_common=False), 'Latin')
 	
 	def test_cjk(self):
-		"""Test uniscript.script() with Han"""
+		"""Test unicodescript.script() with Han"""
 		testcases = u'気氣'
 		for c in testcases:
 			self.assertEqual(script(c, avoid_common=True), 'Han')
 			self.assertEqual(script(c, avoid_common=False), 'Han')
 	
 	def test_hiragana(self):
-		"""Test uniscript.script() with Hiragana"""
+		"""Test unicodescript.script() with Hiragana"""
 		testcases = u'あいうえおはばぱをん'
 		for c in testcases:
 			self.assertEqual(script(c, avoid_common=True), 'Hiragana')
 			self.assertEqual(script(c, avoid_common=False), 'Hiragana')
 	
 	def test_katakana(self):
-		"""Test uniscript.script() with Katakana"""
+		"""Test unicodescript.script() with Katakana"""
 		testcases1 = u'アイウエオハパバヲン'
 		for c in testcases1:
 			self.assertEqual(script(c, avoid_common=False), 'Katakana')
@@ -78,14 +78,14 @@ class TestScript(unittest.TestCase):
 			self.assertEqual(script(c, avoid_common=True), 'Katakana')
 	
 	def test_katakana_halfwidth(self):
-		"""Test uniscript.script() with Katakana halfwidth"""
+		"""Test unicodescript.script() with Katakana halfwidth"""
 		testcases = u'ｶﾀｶﾅ'
 		for c in testcases:
 			self.assertEqual(script(c, avoid_common=False), 'Katakana')
 			self.assertEqual(script(c, avoid_common=True), 'Katakana')
 	
 	def test_latin_fullwidth(self):
-		"""Test uniscript.script() with Latin fullwidth"""
+		"""Test unicodescript.script() with Latin fullwidth"""
 		testcases = u'ａＡ'
 		for c in testcases:
 			self.assertEqual(script(c, avoid_common=False), 'Latin')
