9128df29b13cfbb5d4d0b885badeefbfd01ca1f6
Eric Dessalle <eric.dessalle@gmail.com>
2011-03-08 22:53:46 +0900

    Replace the "tagenwa.util.tools" module by the "tagenwa.utils" module

---
+++ tagenwa/utils.py
@@ -0,0 +1,95 @@
+# -*- coding: UTF-8 -*-
+"""
+Useful functions for tokenization
+
+"""
+import functools
+
+from itertools import tee, chain, izip, izip_longest
+# INFO: izip_longest is new in Python 2.6
+from collections import defaultdict
+
+
+def sliding_tuples(iterable, length, fill_value=None, fill_lead=True, fill_tail=True):
+	"""Generate an iterable of tuples of consecutive items from the iterable.
+	
+	Simple usage:
+	>>> list(sliding_tuples(xrange(4), 3))
+	[(None, None, 0), (None, 0, 1), (0, 1, 2), (1, 2, 3), (2, 3, None), (3, None, None)]
+	
+	Avoiding tuples partially outside of the iterable:
+	>>> list(sliding_tuples(xrange(4), 3, fill_lead=False, fill_tail=False))
+	[(0, 1, 2), (1, 2, 3)]
+	
+	:param iterable: an iterable
+	:param length: the length of the tuples to return
+	:type length: int
+	:param fill_value: the value to used for filling when the tuple is partially outside of the iterable (default is None)
+	:param fill_lead: True if partial tuples should be returned at the beginning (default is True)
+	:type fill_lead: bool
+	:param fill_tail: True if partial tuples should be returned at the end (default is True)
+	:type fill_tail: bool
+	:return: iterable of tuples
+	:rtype: iterator
+	"""
+	
+	# fill lead if needed
+	if fill_lead:
+		iterable = chain([fill_value]*(length-1), iterable)
+	
+	# make a list of n iterables
+	# and initialize each iterable by shifting it according to his index
+	iterables = tee(iterable, length)
+	for i in xrange(1,length):
+		for j in xrange(i):
+			try:
+				iterables[i].next()
+			except StopIteration as e:
+				# leave iterable closed if shorter than 'length'
+				# (izip_longuest will take care of it)
+				pass
+	
+	# fill tail if needed
+	if fill_tail:
+		zipped = izip_longest(*iterables, fillvalue=fill_value)
+	else:
+		zipped = izip(*iterables)
+	
+	# yield the tuples
+	for tu in zipped:
+		yield tu
+
+
+def counts(iterable):
+	"""Return a dictionary of occurences."""
+	d = defaultdict(int)
+	for i in iterable:
+		d[i] += 1
+	return dict(d)
+
+
+def merge_tagged(iterable, merge_tokens=None):
+	"""Return an iterable of tagged tokens where the consecutive tokens having the same tag
+	in the given iterable have been merged."""
+	# Initialize the iteration
+	iterator = iter(iterable)
+	try:
+		token, tag0 = iterator.next()
+		buffer = [token]
+	except StopIteration:
+		# Re-raise the StopIteration in case of empty iterator
+		raise
+	
+	if merge_tokens is None:
+		merge_tokens = lambda x:x
+	
+	# Merge consecutive tokens having the same tag
+	merged_tagged = []
+	for token, tag in iterator:
+		if tag != tag0:
+			yield (merge_tokens(buffer), tag0)
+			buffer = [token]
+			tag0 = tag
+		else:
+			buffer.append(token)
+	yield (merge_tokens(buffer), tag0)
---
+++ test/test_utils.py
@@ -0,0 +1,81 @@
+# -*- coding: UTF-8 -*-
+import unittest, doctest
+
+from tagenwa.utils import sliding_tuples, counts
+
+
+class TestUtils(unittest.TestCase):
+	
+	def test_util_doctest(self):
+		import tagenwa.util
+		failure_count, test_count = doctest.testmod(tagenwa.util)
+		self.assertEqual(failure_count, 0, 'Testing doctest from tagenwa.util: %i failed out of %i' % (failure_count, test_count))
+
+	def test_sliding_tuples(self):
+		testcases = [
+			(
+				('abc', 1),
+				[('a',), ('b',),('c',)]
+			),
+			(
+				('abc', 1, None, False, False),
+				[('a',), ('b',),('c',)]
+			),
+			(
+				('abc', 2),
+				[(None,'a'),('a','b'),('b','c'),('c',None)]
+			),
+			(
+				('abc', 2, None, True, True),
+				[(None,'a'),('a','b'),('b','c'),('c',None)]
+			),
+			(
+				('abc', 2, None, False, False),
+				[('a','b'),('b','c')]
+			),
+			(
+				('abc', 2, None, True, False),
+				[(None,'a'),('a','b'),('b','c')]
+			),
+			(
+				('abc', 2, None, False, True),
+				[('a','b'),('b','c'),('c',None)]
+			),
+			(
+				('abc', 3, None, False, True),
+				[('a','b','c'), ('b','c',None),('c',None,None)]
+			),
+			(
+				('abc', 3, None, True),
+				[(None,None,'a'), (None,'a','b'), ('a','b','c'), ('b','c',None), ('c',None,None)]
+			),
+			(
+				('abc', 4, None, False, True),
+				[('a','b','c',None), ('b','c',None,None),('c',None,None,None)]
+			),
+			(
+				('abc', 4, None, True, True),
+				[(None,None,None,'a'), (None,None,'a','b'), (None,'a','b','c'), ('a','b','c',None), ('b','c',None,None),('c',None,None,None)]
+			),
+			# test fillvalue
+			(
+				('abc', 2, 'x'),
+				[('x','a'), ('a','b'), ('b','c'),('c','x')]
+			),
+			# test with an iterator
+			(
+				(xrange(4), 2),
+				[(None,0),(0,1),(1,2),(2,3),(3,None)]
+			),
+		]
+		for (i,e) in testcases:
+			self.assertEqual(e, list(sliding_tuples(*i)))
+
+
+
+def suite():
+	suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
+	return suite
+
+if __name__ == '__main__':
+	unittest.main()
