48f203102bc001c8bbd46262f7b0656e8045b4f5
Eric Dessalle <eric.dessalle@gmail.com>
2011-05-31 22:49:53 +0900

    Add test for the dictionary-based retokenizer

--- test/test_tokenize_dictionary.py
+++ test/test_tokenize_dictionary.py
@@ -6,15 +6,23 @@ from tagenwa.tokenize.dictionary import DictionaryRetokenizer
 class TestDictionaryRetokenizer(unittest.TestCase):
 	
 	def setUp(self):
-		self.retokenizer = DictionaryRetokenizer()
-		self.retokenizer.add(
-			[u'hello'],
-			[u'HELLO']
-		)
-		self.retokenizer.add(
-			[u'good',u'morning'],
-			[u'GOOD', u'MORNING']
-		)
+		self.retokenizer1 = DictionaryRetokenizer()
+		self.retokenizer2 = DictionaryRetokenizer(allow_overlap=True)
+		
+		for r in (self.retokenizer1, self.retokenizer2):
+			r.add(
+				[u'hello'],
+				[u'HELLO']
+			)
+			r.add(
+				[u'good', u'morning'],
+				[u'GOOD', u'MORNING']
+			)
+			r.add(
+				[u'morning', u'breakfast'],
+				[u'MORNING', u'BREAKFAST']
+			)
+	
 	
 	def test_simple(self):
 		testcases = [
@@ -26,7 +34,9 @@ class TestDictionaryRetokenizer(unittest.TestCase):
 			([u'abc', u'hello', u'xyz'],[u'abc', u'HELLO', u'xyz']),
 		]
 		for i,e in testcases:
-			self.assertEqual(e, list(self.retokenizer.retokenize(i)))
+			self.assertEqual(e, list(self.retokenizer1.retokenize(i)))
+			self.assertEqual(e, list(self.retokenizer2.retokenize(i)))
+	
 	
 	def test_compound(self):
 		testcases = [
@@ -40,7 +50,22 @@ class TestDictionaryRetokenizer(unittest.TestCase):
 			([u'abc', u'morning', u'xyz'],[u'abc', u'morning', u'xyz']),
 		]
 		for i,e in testcases:
-			self.assertEqual(e, list(self.retokenizer.retokenize(i)))
+			self.assertEqual(e, list(self.retokenizer1.retokenize(i)))
+			self.assertEqual(e, list(self.retokenizer2.retokenize(i)))
+	
+	
+	def test_overlap(self):
+		testcases = [
+			([u'abc', u'good',u'morning', u'breakfast'],[u'abc', u'GOOD',u'MORNING', u'breakfast']),
+		]
+		for i,e in testcases:
+			self.assertEqual(e, list(self.retokenizer1.retokenize(i)))
+		
+		testcases = [
+			([u'abc', u'good',u'morning', u'breakfast'],[u'abc', u'GOOD',u'MORNING', u'MORNING', u'BREAKFAST']),
+		]
+		for i,e in testcases:
+			self.assertEqual(e, list(self.retokenizer2.retokenize(i)))
 
 
 
